72241cd3ba8d9bb35bfd8121fb26f387b7b094cd
Anthony G <agithaka@live.com>
2022-08-24 22:21:21 +0300

    refactor: user api controller - me action

--- modules/api/controllers/UserController.php
+++ modules/api/controllers/UserController.php
@@ -2,7 +2,13 @@
 
 namespace app\modules\api\controllers;
 
-class UserController
-{
+use Yii;
 
+class UserController extends base\BaseController
+{
+    public function actionMe()
+    {
+        // Warning: when returning a model make sure you implement its `field()` method (see Exposing data in the Installing the Yii2-Oauth2-Server guide).
+        return Yii::$app->user->identity;
+    }
 }
