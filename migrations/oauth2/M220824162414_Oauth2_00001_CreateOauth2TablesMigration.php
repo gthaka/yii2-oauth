2ba553b2f9b98e85fa7c90a7038be150c9104d28
Anthony G <agithaka@live.com>
2022-08-24 20:01:47 +0300

    oauth2 generated migrations

---
+++ migrations/oauth2/M220824162414_Oauth2_00001_CreateOauth2TablesMigration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\migrations\oauth2;
+
+use rhertogh\Yii2Oauth2Server\migrations\Oauth2_00001_CreateOauth2TablesMigration;
+
+/**
+ * Class app\migrations\oauth2\M220824162414_Oauth2_00001_CreateOauth2TablesMigration
+ *
+ * phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+ * phpcs:disable Generic.Files.LineLength.TooLong
+ */
+class M220824162414_Oauth2_00001_CreateOauth2TablesMigration extends Oauth2_00001_CreateOauth2TablesMigration
+{
+    /**
+     * Wrapper class for rhertogh\Yii2Oauth2Server\migrations\Oauth2_00001_CreateOauth2TablesMigration,
+     * no further implementation required.
+     */
+}
---
+++ migrations/oauth2/M220824162414_Oauth2_00002_InsertOpenIdConnectScopesMigration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\migrations\oauth2;
+
+use rhertogh\Yii2Oauth2Server\migrations\Oauth2_00002_InsertOpenIdConnectScopesMigration;
+
+/**
+ * Class app\migrations\oauth2\M220824162414_Oauth2_00002_InsertOpenIdConnectScopesMigration
+ *
+ * phpcs:disable Squiz.Classes.ValidClassName.NotCamelCaps
+ * phpcs:disable Generic.Files.LineLength.TooLong
+ */
+class M220824162414_Oauth2_00002_InsertOpenIdConnectScopesMigration extends Oauth2_00002_InsertOpenIdConnectScopesMigration
+{
+    /**
+     * Wrapper class for rhertogh\Yii2Oauth2Server\migrations\Oauth2_00002_InsertOpenIdConnectScopesMigration,
+     * no further implementation required.
+     */
+}
