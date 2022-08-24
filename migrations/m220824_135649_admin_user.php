cc76d1253b03e7832ba195c5d2d65ca6c4deca91
Anthony G <agithaka@live.com>
2022-08-24 18:05:16 +0300

    refactor: admin user migration default

--- migrations/m220824_135649_admin_user.php
+++ migrations/m220824_135649_admin_user.php
@@ -12,7 +12,33 @@ class m220824_135649_admin_user extends Migration
      */
     public function safeUp()
     {
+        $auth = Yii::$app->authManager;
 
+        // create a role named "administrator"
+        $administratorRole = $auth->createRole('admin');
+        $administratorRole->description = 'Administrator';
+        $auth->add($administratorRole);
+
+        // create permission for certain tasks
+        $permission = $auth->createPermission('user-management');
+        $permission->description = 'User Management';
+        $auth->add($permission);
+
+        // let administrators do user management
+        $auth->addChild($administratorRole, $auth->getPermission('user-management'));
+
+        // create user "admin" with password "verysecret"
+        $user = new \Da\User\Model\User([
+            'scenario' => 'create',
+            'email' => "email@example.com",
+            'username' => "admin",
+            'password' => "verysecret"  // >6 characters!
+        ]);
+        $user->confirmed_at = time();
+        $user->save();
+
+        // assign role to our admin-user
+        $auth->assign($administratorRole, $user->id);
     }
 
     /**
@@ -20,9 +46,17 @@ class m220824_135649_admin_user extends Migration
      */
     public function safeDown()
     {
-        echo "m220824_135649_admin_user cannot be reverted.\n";
+        $auth = Yii::$app->authManager;
 
-        return false;
+        // delete permission
+        $auth->remove($auth->getPermission('user-management'));
+
+        // delete admin-user and administrator role
+        $administratorRole = $auth->getRole("administrator");
+        $user = \Da\User\Model\User::findOne(['username'=>"admin"]);
+        $auth->revoke($administratorRole, $user->id);
+        $user->delete();
+        $auth->remove($administratorRole);
     }
 
     /*
