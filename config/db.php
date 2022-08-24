985e726a4bc4fb1a2945b4374bd5ce977c6cbfd4
Anthony G <agithaka@live.com>
2022-08-24 13:11:53 +0300

    refactor: db vars to use dotenv vars

--- config/db.php
+++ config/db.php
@@ -1,10 +1,13 @@
 <?php
 
+$db_host = $_ENV['DB_HOST'];
+$db_name = $_ENV['DB_NAME'];
+
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'class' => yii\db\Connection::class,
+    'dsn' => "mysql:host=$db_host;dbname=$db_name",
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
